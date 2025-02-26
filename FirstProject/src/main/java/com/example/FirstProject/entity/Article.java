3d7a831075ee5d737361ad018b8c71536546e041
Hong0723 <166686786+Hong0723@users.noreply.github.com>
2025-02-26 17:57:24 +0900

    2025/02/26
    CRUD와 SQL쿼리

--- FirstProject/src/main/java/com/example/FirstProject/entity/Article.java
+++ FirstProject/src/main/java/com/example/FirstProject/entity/Article.java
@@ -1,16 +1,13 @@
 package com.example.FirstProject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Entity
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
